bfd81f5c4c7a140c78979086cb5e21915eb17c78
Setyo <setyobudi594@gmail.com>
2026-09-07 11:28:25 +0700

    Kembalikan tampilan ToolKu ke versi 19f8b93

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,28 +1,45 @@
-const CACHE_NAME = "toolku-v1";
+const CACHE_NAME = "toolku-v4";
 
-const FILES = [
+const FILES_TO_CACHE = [
     "./",
     "./index.html",
+    "./manifest.json",
+    "./splash.css",
+    "./bottom-nav.css",
+    "./service-worker.js",
     "./kalkulator.html",
     "./qrcode.html",
-    "./resize.html",
-    "./password.html",
     "./catatan.html",
-    "./manifest.json"
+    "./password.html",
+    "./resize.html",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(FILES);
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
         })
     );
 });
 
 self.addEventListener("fetch", event => {
     event.respondWith(
-        caches.match(event.request).then(cached => {
-            return cached || fetch(event.request);
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
         })
     );
 });
